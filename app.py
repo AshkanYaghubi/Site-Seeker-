906efa325aa75fd81941ba2ee35f58a07022f7a4
AshkanYaghubi <137271729+AshkanYaghubi@users.noreply.github.com>
2023-09-17 15:23:48 +0330

    Update app.py

--- app.py
+++ app.py
@@ -6,26 +6,6 @@ from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
 from cv2 import imread, resize
 from numpy import argmax, array, newaxis
-from keras.layers import Dropout
-from keras import backend as K
-from keras.utils import CustomObjectScope
-
-
-"""
-class FixedDropout(Dropout):
-    def __init__(self, rate, noise_shape=None, seed=None, **kwargs):
-        super(FixedDropout, self).__init__(rate, noise_shape, seed, **kwargs)
-        self.uses_learning_phase = False
-
-    def call(self, x, mask=None):
-        if 0. < self.rate < 1.:
-            noise_shape = self._get_noise_shape(x)
-            x = K.dropout(x, self.rate, noise_shape,
-                          seed=self.seed)
-        return x
-"""
-#with CustomObjectScope({'FixedDropout': FixedDropout}):
-model = load_model('vgg16.h5')
 
 UPLOAD_FOLDER = 'uploads'
 SECRET_KEY = "SECRETKEY"
@@ -39,18 +19,9 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def home():
     return render_template("home.html")
 
-@app.route("/blog")
-def blog():
-    return render_template("blog.html")
-
-@app.route("/blog/post_1")
-def blog_post_1():
-    return render_template("/posts/blog_post_1.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/<filename>")
 def uploads(filename):
@@ -59,24 +30,22 @@ def uploads(filename):
 
 @app.route("/predict", methods = ["GET", "POST"])
 def predict():
-    result = ''
     form = mainform()
     UPLOADED_FOLDER = "uploads"
     if form.validate_on_submit():
         file = form.image.data
 
         if file:
-            result = ''
             filename = secure_filename(file.filename)
             file.save(os.path.join('static',app.config['UPLOAD_FOLDER'], filename))
             img = imread(os.path.join('static',app.config['UPLOAD_FOLDER'], filename),1)
             img = resize(img,(224,224))
             X = array(img)
             x_2d = X[newaxis, :]
+            model = load_model('vgg16.h5')
             class_names = ['ImamReza', 'Milad', 'Pasargadae', 'Persepolis']
-            #class_names = ['Persepolis', 'Pasargadae', 'Milad', 'ImamReza']
             result = class_names[argmax(model.predict(x_2d))]
-            return render_template('predict.html', form = form, result = result, filename = filename)
+            return render_template('predict.html', form = form, filename = filename, result = result)
 
     return render_template('predict.html', form = form, filename = None)
 
